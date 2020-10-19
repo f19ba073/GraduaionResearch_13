7db6bb429df1e2aecbf5430cb21b119fe60baadf
msk <61035772+mtid-r@users.noreply.github.com>
2020-10-19 12:21:14 +0900

    game_play.xmlとつなげてみた。 (#1)

---
+++ app/src/main/java/com/example/graduaionresearch_13/GamePlayActivity.java
@@ -0,0 +1,14 @@
+package com.example.graduaionresearch_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_play);
+    }
+}
