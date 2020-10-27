392b8bcad6b47b1cb3a4f72c922a1b455280741a
f19ba073 <64409053+f19ba073@users.noreply.github.com>
2020-10-27 09:50:51 +0900

    MainActivityに画面遷移機能を追加しました (#5)

--- app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.graduaionresearch_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        final Button start_button = findViewById(R.id.start_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), VocabularyBookActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
