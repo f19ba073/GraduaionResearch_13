1b24372b75fe1bde51a2edbfb86ee5fc9c1fb122
f19ba055 <66896349+f19ba055@users.noreply.github.com>
2020-11-05 10:16:13 +0900

    problemクラスを作成しました。 (#9)

---
+++ app/src/main/java/com/example/graduaionresearch_13/problem.java
@@ -0,0 +1,47 @@
+package com.example.graduaionresearch_13;
+
+public class problem {
+    private int id;
+    private String problem;
+    private String answer;
+    private int book_id;
+
+    public problem(int id,String problem,String answer,int book_id){
+        this.id=id;
+        this.problem=problem;
+        this.answer=answer;
+        this.book_id=book_id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getProblem(){
+        return this.problem;
+    }
+
+    public String getAnswer(){
+        return this.answer;
+    }
+
+    public int getBook_id(){
+        return this.book_id;
+    }
+
+    public void setId(int id){
+        this.id=id;
+    }
+
+    public void setProblem(String problem){
+        this.problem=problem;
+    }
+
+    public void setAnswer(String answer){
+        this.answer=answer;
+    }
+
+    public void setBook_id(int book_id){
+        this.book_id=book_id;
+    }
+}
