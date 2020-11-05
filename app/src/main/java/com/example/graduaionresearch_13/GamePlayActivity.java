81cd2dac9ba0808269af6dc466c877f4b2e1fa00
f19ba036 <68531913+f19ba036@users.noreply.github.com>
2020-11-05 10:22:45 +0900

    ゲームプレイ画面の作成をしました。 (#10)
    * ゲームプレイ画面の作成をしました。
    
    * javaの中身の変更をしました。

--- app/src/main/java/com/example/graduaionresearch_13/GamePlayActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/GamePlayActivity.java
@@ -2,7 +2,10 @@ package com.example.graduaionresearch_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GamePlayActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class GamePlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_play);
+
+        final Button start_button = findViewById(R.id.gamestart_button);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), GameProduction.class);
+                startActivity(intent);
+            }
+        });
     }
 }
