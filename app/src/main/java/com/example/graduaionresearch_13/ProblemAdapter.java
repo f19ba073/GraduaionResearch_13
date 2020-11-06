8b769eb015f6acec47f6c820f2c905315e326079
f19ba073 <64409053+f19ba073@users.noreply.github.com>
2020-11-06 10:40:29 +0900

    問題一覧画面を作成し、削除イベントを実装しました。 (#14)

--- app/src/main/java/com/example/graduaionresearch_13/ProblemAdapter.java
+++ app/src/main/java/com/example/graduaionresearch_13/ProblemAdapter.java
@@ -1,20 +1,62 @@
 package com.example.graduaionresearch_13;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import java.util.List;
+import java.util.*;
 
 public class ProblemAdapter extends BaseAdapter {
     private LayoutInflater inflater;
     private int resourcedId;
     private List<Problem> problemList;
 
+    private static class ViewHolder {
+        TextView textView;
+        Button menuButton;
+    }
+
+    public ProblemAdapter(Context context, int resourcedId, List<Problem> list){
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.resourcedId = resourcedId;
+        this.problemList = list;
+    }
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        ViewHolder holder;
+        if(convertView == null){
+            convertView = inflater.inflate(resourcedId,parent,false);
+
+            holder = new ViewHolder();
+
+            holder.textView = convertView.findViewById(R.id.problem_text);
+            holder.menuButton = convertView.findViewById(R.id.problem_menu);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.textView.setText(problemList.get(position).getProblem());
+        holder.menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("onClick","子要素でクリックされました");
+                ((ListView) parent).performItemClick(view, position, R.id.problem_menu);
+            }
+        });
+
         return convertView;
+    }
+
+    public void delete(int position){
+        problemList.remove(position);
     }
 
     @Override
@@ -24,12 +66,12 @@ public class ProblemAdapter extends BaseAdapter {
 
     @Override
     public Object getItem(int position) {
-        return position;
+        return problemList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return position;
+        return problemList.get(position).getId();
     }
 
 }
--- app/src/main/java/com/example/graduaionresearch_13/ProblemListActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/ProblemListActivity.java
@@ -2,7 +2,11 @@ package com.example.graduaionresearch_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.*;
@@ -13,6 +17,7 @@ public class ProblemListActivity extends AppCompatActivity {
             new Problem(2,"jdkとは","java開発環境",1),
             new Problem(3,"javacとは","コンパイル",1)
     ));
+    private ProblemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,66 @@ public class ProblemListActivity extends AppCompatActivity {
         setContentView(R.layout.problem_list);
 
         ListView listView = (ListView) findViewById(R.id.problem_listView);
+        adapter = new ProblemAdapter(getApplicationContext(), R.layout.problem_row, problmes);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                final String[] items = {"編集", "削除", "キャンセル"};
+                new AlertDialog.Builder(ProblemListActivity.this)
+                        .setTitle("Selector")
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which){
+                                    case 0:
+                                        edit(position);
+                                        break;
+                                    case 1:
+                                        deleteCheck(position);
+                                        break;
+                                    case 2:
+                                        break;
+                                }
+                            }
+                        })
+                        .show();
+            }
+        });
+    }
+
+    private void deleteCheck(final int position) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ProblemListActivity.this)
+                .setTitle("削除").setMessage("本当に削除しますか？");
+
+        // AlertDialogのYesボタンのコールバックリスナーを登録
+        alertDialogBuilder.setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        delete(position);
+                    }
+                });
+        // AlertDialogのNoボタンのコールバックリスナーを登録
+        alertDialogBuilder.setNeutralButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+        // AlertDialogのキャンセルができるように設定
+        alertDialogBuilder.setCancelable(true);
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // AlertDialogの表示
+        alertDialog.show();
+    }
+
+
+    private void delete(int position){
+        adapter.delete(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void edit(int position){
 
     }
 }
