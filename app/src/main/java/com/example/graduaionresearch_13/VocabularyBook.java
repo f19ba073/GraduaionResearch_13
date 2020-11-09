1048ba77e498a41613c6ce53cc8d2fd8d5160122
f19ba065 <68838858+f19ba065@users.noreply.github.com>
2020-11-09 11:24:55 +0900

    VocabularyBook.javaファイルに追加 (#26)

--- app/src/main/java/com/example/graduaionresearch_13/VocabularyBook.java
+++ app/src/main/java/com/example/graduaionresearch_13/VocabularyBook.java
@@ -1,5 +1,19 @@
 package com.example.graduaionresearch_13;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.graduaionresearch_13.DBNames.COLUMN_NAME_BOOK_ID;
+import static com.example.graduaionresearch_13.DBNames.COLUMN_NAME_BOOK_NAME;
+import static com.example.graduaionresearch_13.DBNames.COLUMN_NAME_ID;
+import static com.example.graduaionresearch_13.DBNames.PROBLEM_COLUMNS;
+import static com.example.graduaionresearch_13.DBNames.TABLE_NAME_BOOKS;
+import static com.example.graduaionresearch_13.DBNames.TABLE_NAME_PROBLEMS;
+
 public class VocabularyBook {
     private int book_id;
     private String book_name;
@@ -9,6 +23,22 @@ public class VocabularyBook {
         this.book_name=book_name;
     }
 
+    public static List<VocabularyBook> getList(Context context){
+        List<VocabularyBook> list = new ArrayList<>();
+        DBOpenHelper helper = new DBOpenHelper(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        Cursor c = db.query(TABLE_NAME_BOOKS,new String[]{COLUMN_NAME_ID,COLUMN_NAME_BOOK_NAME},
+                null, null, null, null,null);
+        if(c == null){return list;}
+        try{
+            while(c.moveToNext()){
+                list.add(new VocabularyBook(c.getInt(0),c.getString(1)));
+            }
+        }finally {
+            c.close();
+        }
+        return list;
+    }
     public int getBook_id(){
         return this.book_id;
     }
