91fd18f5bd2792b93e9276d6ca44cc27c9e167ef
TakumiMiyamoto38 <f19ba073@gmail.com>
2020-11-14 12:39:34 +0900

    ゲームプレイ画面に正誤判定処理を追加しました

--- app/src/main/java/com/example/graduaionresearch_13/GamePlayActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/GamePlayActivity.java
@@ -2,49 +2,84 @@ package com.example.graduaionresearch_13;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class GamePlayActivity extends AppCompatActivity implements View.OnClickListener{
+public class GamePlayActivity extends AppCompatActivity{
+
     private final List<Problem> problmes = new ArrayList<>(Arrays.asList(
             new Problem(1,"jvmとは","java仮想マシン",1),
             new Problem(2,"jdkとは","java開発環境",1),
             new Problem(3,"javacとは","コンパイル",1)
     ));
-    private Button answer_button;
-    private TextView textview1;
-    private int index;
+    private int problemsIndex;
 
+    private Button nextTransitionButton;
+    private Button endButton;
+    private TextView problemTextView;
+    private EditText editAnswerText;
+
+    private OnClickNextProblem onClickNextProblem = new OnClickNextProblem();
+    private OnClickResultCheck onClickResultCheck = new OnClickResultCheck();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_play);
-        index = 0;
+        problemsIndex = 0;
 
         final Button start_button = findViewById(R.id.gamestart_button);
         start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setContentView(R.layout.game_production);
-                answer_button = findViewById(R.id.nextproblem_button);
-                answer_button.setOnClickListener(GamePlayActivity.this);
-                textview1=(TextView)findViewById(R.id.textView);
+                nextTransitionButton = findViewById(R.id.next_transition_button);
+                endButton = findViewById(R.id.end_button);
+                problemTextView = findViewById(R.id.problem_text_view);
+                editAnswerText = findViewById(R.id.edit_answer_text);
+
+                setProblemWithIndex(problemsIndex);
+                nextTransitionButton.setOnClickListener(onClickResultCheck);
             }
         });
     }
 
-    @Override
-    public void onClick(View V) {
-        textview1.setText(problmes.get(index).getProblem());
-        index++;
+    private void setProblemWithIndex(int index){
+        problemTextView.setText(problmes.get(index).getProblem());
+    }
+
+    private class OnClickNextProblem implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            nextTransitionButton.setText("正解へ");
+            setProblemWithIndex(problemsIndex);
+            nextTransitionButton.setOnClickListener(onClickResultCheck);
+        }
+    }
+
+    private class OnClickResultCheck implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            if(editAnswerText.getText().equals(problmes.get(problemsIndex))){
+                //正解
+                Log.d("answer","正解");
+            }else{
+                //不正解
+                Log.d("answer","不正解");
+            }
+
+            nextTransitionButton.setText("次の問題へ");
+            nextTransitionButton.setOnClickListener(onClickNextProblem);
+            problemsIndex++;
+        }
     }
 }
