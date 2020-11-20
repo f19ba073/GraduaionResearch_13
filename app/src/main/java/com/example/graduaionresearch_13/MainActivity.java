3a07fcd37f928edc575d408c5edf6efbbf661e0e
f19ba065 <f19ba065@gmail.com>
2020-11-20 11:26:21 +0900

    木の成長イラスト作成

--- app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        int[] percent = {0,20,40,60,80};
+        ImageView imageView1 = findViewById(R.id.image_view_1);
+
+        switch(percent[0]/20) {
+            case 0:
+                imageView1.setImageResource(R.drawable.faketree);
+                break;
+            case 1:
+                imageView1.setImageResource(R.drawable.genkinaki);
+                break;
+            case 2:
+                imageView1.setImageResource(R.drawable.faketree);
+                break;
+            case 3:
+                imageView1.setImageResource(R.drawable.genkinaki);
+                break;
+            case 4:
+                imageView1.setImageResource(R.drawable.faketree);
+                break;
+        }
 
         final Button start_button = findViewById(R.id.start_button);
         start_button.setOnClickListener(new View.OnClickListener() {
