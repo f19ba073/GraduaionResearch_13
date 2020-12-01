0404451272403a33c4a6e52e31e7ef904e9e9342
TakumiMiyamoto38 <f19ba073@gmail.com>
2020-12-01 12:29:01 +0900

    toolbarのバグを修正しました

--- app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
+++ app/src/main/java/com/example/graduaionresearch_13/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
